5e60a8b2a3b564445c188e5704b1706bc490c9a5
Kristian Joos <kjoos22@gmail.com>
2020-09-16 22:32:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+require 'pry'
 
+def sort_array_asc(numbers)
+  numbers = numbers.sort
+end
+
+def sort_array_desc(numbers)
+  numbers = numbers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings = strings.sort_by {|string| string.length}
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(strings)
+  new_strings = strings.each do |string|
+    string[2] = "$"
+  end
+  new_strings
+end
+
+def find_a(strings)
+  i = 0
+  strings.map do |string|
+    if string[0] != "a"
+      x = 1
+    end
+  end
+  strings
+end
